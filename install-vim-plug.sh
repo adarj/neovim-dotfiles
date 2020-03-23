8b086f2d2e0a0377a33c508f966a73474894e4a5
Akil Darjean <akil.darjean@searchspring.com>
2020-03-23 09:41:00 -0500

    Remove installer for Vim

--- install-vim-plug.sh
+++ install-vim-plug.sh
@@ -1,7 +1,2 @@
-# Vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
